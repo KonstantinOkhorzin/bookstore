6e3c29955b14a9a3226a9319d0daeb919ec61b8e
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-06-29 11:08:30 +0300

    feat(server): add isValidId middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -2,3 +2,4 @@ export { default as validateBody } from './validateBody.js';
 export { default as uploadFile } from './uploadFile.js';
 export { default as authenticate } from './authenticate.js';
 export { default as isAdmin } from './isAdmin.js';
+export { default as isValidId } from './isValidId.js';
