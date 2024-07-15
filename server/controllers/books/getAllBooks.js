d5be1b349dd8b8e76c1108ada74946c27b7fb85b
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-07-15 07:20:40 +0300

    feat(server): add search by title to getAllBooks

--- server/controllers/books/getAllBooks.js
+++ server/controllers/books/getAllBooks.js
@@ -2,13 +2,17 @@ import Book from '../../models/book.js';
 import { blackList } from './сonfig.js';
 
 const getAllBooks = async (req, res) => {
-  const { page = 1, limit = 8 } = req.query;
+  const { page = 1, limit = 8, bookTitle = '' } = req.query;
   const parsedPage = Number(page);
   const parsedLimit = Number(limit);
+  const searchQuery = bookTitle ? { title: new RegExp(bookTitle, 'i') } : {};
 
   const skip = (parsedPage - 1) * parsedLimit;
-  const totalBooks = await Book.countDocuments();
-  const books = await Book.find({}, blackList, { skip, limit: parsedLimit });
+  const totalBooks = await Book.countDocuments(searchQuery);
+  const books = await Book.find(searchQuery, blackList, {
+    skip,
+    limit: parsedLimit,
+  });
 
   const totalPages = Math.ceil(totalBooks / parsedLimit);
   const hasPrevPage = parsedPage > 1;
