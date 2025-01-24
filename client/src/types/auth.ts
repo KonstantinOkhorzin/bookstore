397b0c4468a604ca348d9a98ca519c90b15e5eec
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-01-24 16:18:21 +0200

    feat(client)(types): add auth-related types

---
+++ client/src/types/auth.ts
@@ -0,0 +1,36 @@
+export interface IUser {
+  email: string;
+  name: string;
+  avatarURL: string;
+  role: string;
+}
+
+export interface IToken {
+  token: string;
+}
+
+export interface IUserAuthResponse extends IToken {
+  user: IUser;
+}
+
+export interface IErrorResponse {
+  message: string;
+}
+
+export interface IAuthRequest {
+  email: string;
+  password: string;
+}
+
+export interface IRegisterRequest extends IAuthRequest {
+  name: string;
+  avatar?: File;
+}
+
+export interface IUpdateAvatarRequest {
+  avatar: File;
+}
+
+export interface IUpdateAvatarResponse {
+  avatarURL: string;
+}
