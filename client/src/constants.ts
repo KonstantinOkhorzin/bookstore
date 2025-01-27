6f93d8ebaaa9c680d6c56dd67ad3bb9998f795b7
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-01-27 08:45:12 +0200

    feat(client)(constants): update routes and add API endpoints, HTTP methods, and tags

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -7,5 +7,37 @@ export const ROUTES = {
   FAVORITES: 'favorites',
   CART: 'cart',
   NOT_FOUND: '*',
-  ADMIN_DASHBOARD: '/admin-dashboard',
+  ADMIN_DASHBOARD: '/admin',
+};
+
+export const BOOKS_API_ENDPOINTS = {
+  USERS: {
+    SIGN_UP: 'users/register',
+    SIGN_IN: 'users/login',
+    SIGN_OUT: 'users/logout',
+    CURRENT: 'users/current',
+    AVATARS: 'users/avatars',
+  },
+  BOOKS: {
+    GET_ALL: 'books',
+    GET_BY_ID: (id: string) => `books/${id}`,
+    CREATE: '/api/books',
+    DELETE_BY_ID: (id: string) => `books/${id}`,
+  },
+};
+
+export const HTTP_METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  DELETE: 'DELETE',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+};
+
+export const TAG_TYPES = {
+  BOOKS: 'Books',
+};
+
+export const TAG_IDS = {
+  LIST: 'LIST',
 };
