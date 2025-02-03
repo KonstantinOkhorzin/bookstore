e531471b13ed25c415bff52f259a5878b053a703
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-02-03 12:40:33 +0200

    feat(client): add global styles using Emotion

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Global } from '@emotion/react';
 
 import App from './App.tsx';
 import store from './redux/store';
+import { globalStyles } from './styles/globalStyles.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Global styles={globalStyles} />
     </Provider>
   </StrictMode>
 );
