113cdeb4b104d39e8374ffa8dce24542e3b3f3fd
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-03-01 11:17:14 +0200

    feat(client): create Books page

--- client/src/components/SharedLayout/SharedLayout.tsx
+++ client/src/components/SharedLayout/SharedLayout.tsx
@@ -8,8 +8,8 @@ const SharedLayout = () => {
   return (
     <Box sx={{ minHeight: '100vh', display: 'grid', gridTemplateRows: 'auto 1fr auto' }}>
       <Header />
-      <Box component='main' px={2} m='0 auto' width='100%' maxWidth='1600px'>
-        <Suspense fallback={<CircularProgress sx={{ margin: '0 auto' }} />}>
+      <Box component='main' px={2} py={4} m='0 auto' width='100%' maxWidth='1100px'>
+        <Suspense fallback={<CircularProgress sx={{ margin: '0 auto', display: 'block' }} />}>
           <Outlet />
           <ScrollRestoration getKey={location => location.pathname} />
         </Suspense>
--- client/src/pages/Books/Books.tsx
+++ client/src/pages/Books/Books.tsx
@@ -1,5 +1,65 @@
+import { ChangeEvent, useMemo } from 'react';
+import { Pagination, Box, CircularProgress, Typography } from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
+
+import { useGetBooksQuery } from '../../redux/apis/books';
+import { handleError } from '../../helpers';
+import { DEFAULTS, QUERY_PARAMS } from '../../constants';
+import { FiltersPanel, BookList } from './components';
+
 const Books = () => {
-  return <div>Books</div>;
+  const { BOOKS_LIMIT, INITIAL_PAGE } = DEFAULTS;
+  const {PAGE} = QUERY_PARAMS;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get(PAGE) ?? INITIAL_PAGE);
+  const params = useMemo(() => Object.fromEntries([...searchParams]), [searchParams]);
+  const { data, error, isFetching, isSuccess } = useGetBooksQuery({
+    limit: BOOKS_LIMIT,
+    page: currentPage,
+    ...params,
+  });
+
+  const onPaginationChange = (_: ChangeEvent<unknown>, page: number) => {
+    const params = Object.fromEntries([...searchParams]);
+    params[PAGE] = page.toString();
+
+    setSearchParams(params);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+      {isSuccess && <FiltersPanel />}
+
+      {isFetching && <CircularProgress sx={{ margin: '0 auto', display: 'block' }} />}
+
+      {error && (
+        <Typography variant='h3' component='p' m='0 auto' textAlign='center' color='error'>
+          {handleError(error)}
+        </Typography>
+      )}
+
+      {data && !isFetching && (
+        <>
+          {data.books.length > 0 ? (
+            <BookList bookList={data.books} />
+          ) : (
+            <Typography variant='h6' component='p' textAlign='center' color='warning'>
+              Books not found! Please try again with different criteria.
+            </Typography>
+          )}
+
+          {data && data.totalBooks > BOOKS_LIMIT && (
+            <Pagination
+              count={data.totalPages}
+              page={currentPage}
+              onChange={onPaginationChange}
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            />
+          )}
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default Books;
