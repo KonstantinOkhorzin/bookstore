eb0ee5bd1d4e99d47acd50a32e3837c52164039e
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-03-06 11:41:17 +0200

    feat(client): create SingleBook page

--- client/src/pages/SingleBook/SingleBook.tsx
+++ client/src/pages/SingleBook/SingleBook.tsx
@@ -1,5 +1,24 @@
+import { useParams } from 'react-router-dom';
+
+import { useGetBookByIdQuery } from '../../redux/apis/books';
+import DetailsBook from './components/DetailsBook';
+import { ErrorMessage, Spinner } from '../../components';
+
 const SingleBook = () => {
-  return <div>Single Book</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetBookByIdQuery(id ?? '', {
+    skip: !id,
+  });
+
+  return (
+    <>
+      {isFetching && <Spinner />}
+
+      {data && <DetailsBook book={data} />}
+
+      {error && <ErrorMessage error={error} />}
+    </>
+  );
 };
 
 export default SingleBook;
